c7740cb5585cc29cb6b69c5da534a80ce78ab521
aamuhic <155601277+aamuhic@users.noreply.github.com>
2024-02-06 09:30:25 -0800

    up

--- 38quiz.py
+++ 38quiz.py
@@ -1,2 +1,18 @@
 # 38quiz.py by Amina Muhic, Francesa C. Gasperini, and Isha Suba
 
+for i in range(1, 7, 2):
+	d = i + 2
+	print(d)
+
+sign = 1
+for i in range(1, 7, 2):
+	sign = sign * -1
+	print(sign)
+	
+pi = 1
+sign = 1
+for i in range(1, 3, 2):
+	sign = sign * -1
+	d = i + 2
+	pi = pi + (1 / d) * sign
+print(4 * pi)
